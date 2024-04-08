91d4b7e1307e624b299d5ac96103ac80b199b0ac
Neander de Souza <neander.faria@hotmail.com>
2024-04-08 14:15:36 -0300

    Migrations no Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: "pg",
+  dbCredentials: {
+    connectionString: "postgres://postgres:s4020@localhost:5432/pizza_shop",
+  },
 } satisfies Config;
