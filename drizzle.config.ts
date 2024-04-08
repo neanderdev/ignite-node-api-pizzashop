1c6d0fe408fffd660c290aad236de93091b6a3b8
Neander de Souza <neander.faria@hotmail.com>
2024-04-08 14:20:07 -0300

    Variáveis ambiente no Bun

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "drizzle-kit";
 
+import { env } from "./src/env";
+
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: "postgres://postgres:s4020@localhost:5432/pizza_shop",
+    connectionString: env.DATABASE_URL,
   },
 } satisfies Config;
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,14 +1,16 @@
+import chalk from "chalk";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const connection = postgres(
-  "postgres://postgres:s4020@localhost:5432/pizza_shop",
-  { max: 1 }
-);
+import { env } from "../env";
+
+const connection = postgres(env.DATABASE_URL, { max: 1 });
 const db = drizzle(connection);
 
 await migrate(db, { migrationsFolder: "drizzle" });
+
+console.log(chalk.greenBright("Migrations applied successfully!"));
 
 await connection.end();
 
