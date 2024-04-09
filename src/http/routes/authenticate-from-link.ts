9433235f8ed387a85756ba83d3c1de26a386dd90
Neander de Souza <neander.faria@hotmail.com>
2024-04-09 10:47:31 -0300

    Rota: Logout e derivação

--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -9,7 +9,7 @@ import { auth } from '../auth'
 
 export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt, cookie: { auth }, set }) => {
+  async ({ query, signUser, set }) => {
     const { code, redirect } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -37,16 +37,9 @@ export const authenticateFromLink = new Elysia().use(auth).get(
       },
     })
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restauranteId: managedRestaurante?.id,
-    })
-
-    auth.set({
-      value: token,
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
     })
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
