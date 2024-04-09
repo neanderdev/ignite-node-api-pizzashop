6da38ff7f8431457719a0c72eb1c4d8b935bd461
Neander de Souza <neander.faria@hotmail.com>
2024-04-09 15:27:40 -0300

    Rota: Restaurante gerenciado

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,7 @@
 import { Elysia } from 'elysia'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getManagedRestaurante } from './routes/get-managed-restaurante'
 import { getProfile } from './routes/get-profile'
 import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
@@ -12,6 +13,7 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
+  .use(getManagedRestaurante)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
