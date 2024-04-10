2899a255a788d3625b8a0b97959349284e2c4ce0
Neander de Souza <neander.faria@hotmail.com>
2024-04-10 10:18:49 -0300

    Rota: Métrica de pedidos cancelados

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,7 @@ import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getDayOrdersAmount } from './routes/get-day-orders-amount'
 import { getManagedRestaurante } from './routes/get-managed-restaurante'
+import { getMonthCanceledOrdersAmount } from './routes/get-month-canceled-orders-amount'
 import { getMonthOrdersAmount } from './routes/get-month-orders-amount'
 import { getMonthReceipt } from './routes/get-month.receipt'
 import { getOrderDetails } from './routes/get-order-details'
@@ -32,6 +33,7 @@ const app = new Elysia()
   .use(getMonthReceipt)
   .use(getDayOrdersAmount)
   .use(getMonthOrdersAmount)
+  .use(getMonthCanceledOrdersAmount)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
